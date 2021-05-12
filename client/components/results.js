2fcf5c7222946edb8d15c09f39c7ecb9d8d9b34f
Elijah Sciammas <eli.sciammas@gmail.com>
2021-05-12 16:16:47 -0400

    Feat: Fade in & material-UI list

--- client/components/results.js
+++ client/components/results.js
@@ -1,13 +1,23 @@
 //useEffect database query to get last 10 calculations
 // //passing results as props to display
-import React from 'react'
+import React from 'react';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Fade from '@material-ui/core/Fade';
+
+
 
 const Results = (props) => {
   return (
     <div>
+      <List component='results'>
      {props.results ? props.results.map((result) => {
-       return <p key={result}>{result}</p>
+       return <Fade key={result} in={true}><ListItem key={result}>
+         <ListItemText primary={result} />
+       </ListItem></Fade>
      }) : <p>Nothing yet!</p>}
+     </List>
     </div>
   )
 }
