36c6e0a161f94272559edac69a0bb318fe21b9d7
Elijah Sciammas <eli.sciammas@gmail.com>
2021-05-12 15:48:09 -0400

    Deployment bugs

--- server/index.js
+++ server/index.js
@@ -71,10 +71,10 @@ const createApp = () => {
   app.use(express.static(path.join(__dirname, '../public')))
 
   // // any remaining requests with an extension (.js, .css, etc.) send 404
-  app.use((req, res, next) => {
+  app.use((err, req, res, next) => {
     if (path.extname(req.path).length) {
-      const err = new Error('Not found')
-      err.status = 404
+      console.log(err)
+      // err.status = 404
       next(err)
     } else {
       next()
